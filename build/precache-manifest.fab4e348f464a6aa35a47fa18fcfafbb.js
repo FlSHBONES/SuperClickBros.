1d6c4b199de8c4f073ecad359c8919423f393e5f
Tim Fisher <tgfisher26@gmail.com>
2018-12-21 10:59:12 -0500

    Pushing the Click Squad

---
+++ build/precache-manifest.fab4e348f464a6aa35a47fa18fcfafbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19f0b17e07cc80b57d28",
+    "url": "/SuperClickBros./static/js/runtime~main.19f0b17e.js"
+  },
+  {
+    "revision": "e1148c336a74d6fced72",
+    "url": "/SuperClickBros./static/js/main.e1148c33.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/SuperClickBros./static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "e1148c336a74d6fced72",
+    "url": "/SuperClickBros./static/css/main.404bcafb.chunk.css"
+  },
+  {
+    "revision": "b83149afc734966cd0feed43ecf11f36",
+    "url": "/SuperClickBros./index.html"
+  }
+];
---
+++ src/components/header/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./header.css";
+
+const Header = props => (
+  <div className="header">
+    <div className="title">{props.children}</div>
+    <div className="scores">
+      Your Score: {props.score}   Top score: {props.highscore}
+    </div>
+  </div>
+
+
+
+
+
+
+
+);
+
+export default Header;
